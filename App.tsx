4a1eee210c36b94f8787966bcf6a4f0895462d60
Vincy-fivestones <vincy.c@fivestones.net>
2023-01-10 15:30:37 +0800

    remove comment

--- App.tsx
+++ App.tsx
@@ -8,14 +8,10 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App() {
   return (
-    // <SafeAreaView className="bg-white flex-1">
-    //   <Text>Open up App.tsx to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </SafeAreaView>
     <SafeAreaProvider>
       <Provider store={store}>
         <PersistGate loading={<Text>Loading ...</Text>} persistor={persistor}>
-          <Board></Board>
+          <Board />
         </PersistGate>
       </Provider>
     </SafeAreaProvider>
